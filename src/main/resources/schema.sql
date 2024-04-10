01cc1bc4dc9c6eced0bdab8eb82cb2c73a11f55c
bardlinga <balin6546@oslomet.no>
2024-04-10 13:11:32 +0200

    format schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,11 +3,11 @@ drop table if exists billett;
 create table billett
 (
     billettId int auto_increment not null,
-    film      varchar(255) not null,
-    antall    int          not null,
-    fornavn   varchar(255) not null,
-    etternavn varchar(255) not null,
-    telefonnr int          not null,
-    epost     varchar(255) not null,
+    film      varchar(255)       not null,
+    antall    int                not null,
+    fornavn   varchar(255)       not null,
+    etternavn varchar(255)       not null,
+    telefonnr int                not null,
+    epost     varchar(255)       not null,
     primary key (billettId)
 );
